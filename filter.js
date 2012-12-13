658a7b578ce8527ca31b6dc89ebede0eb5020666
Caroline <ccragin90@gmail.com>
2012-12-13 00:36:49 -0800

    add feature variation

--- filter.js
+++ filter.js
@@ -2,12 +2,24 @@ var map = mapbox.map('map');
 map.addLayer(mapbox.layer().id('http://a.tiles.mapbox.com/v3/crags617.cragin_homes_blnk.jsonp'));
 var markerLayer = mapbox.markers.layer();
 mapbox.markers.interaction(markerLayer);
-/*map.addLayer(markerLayer);
-map.zoom(2).center({lat:25, lon:-43});*/
-
-map.addLayer(mapbox.markers.layer().markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'));
+map.addLayer(markerLayer);
 map.zoom(2).center({lat:25, lon:-43});
-//markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson');
+
+markerLayer.add_feature({
+    "geometry": {
+        coordinates: [-77, 37.9]
+    },
+    "properties": {
+        'marker-color': '#000',
+        'marker-symbol': 'star-stroked',
+        "title": 'Example Marker',
+        "description": 'This is a single marker.'
+    }
+});
+
+  
+
+//map.addLayer(mapbox.markers.layer().markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'));
 
 /*var markerLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'), function() {
 

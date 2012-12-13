05aabc5d9cfcebe9dab94f1c1ec11bdee2dc01c1
Caroline <ccragin90@gmail.com>
2012-12-13 00:33:22 -0800

    Updated filter.js

--- filter.js
+++ filter.js
@@ -5,41 +5,37 @@ var markerLayer = mapbox.markers.layer();
 map.addLayer(markerLayer);
 map.zoom(2).center({lat:25, lon:-43});*/
 
-map.addLayer(markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'));
-//map.addLayer(markerLayer);?
-      //markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson');
-  /*var markerLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'), function() {
+map.addLayer(mapbox.markers.layer().markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'));
 
-      map.extent(markerLayer.extent());
+//markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson');
 
-      var tweten = document.getElementById('twenty-ten');
-      var all = document.getElementById('ever');
+/*var markerLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'), function() {
 
-      tweten.onclick = function(e) {
-          all.className = '';
-          this.className = 'active';
-          // The filter function takes a GeoJSON feature object
-          // and returns true to show it or false to hide it.
-          markerLayer.filter(function(f) {
-              return f.properties['start_year'] === '2010';
-              //return f.properties['start_year'] == 2010;
-          });
-          return false;
-      };
+    map.extent(markerLayer.extent());
 
-      all.onclick = function() {
-          tweten.className = '';
-          this.className = 'active';
-          markerLayer.filter(function(f) {
-              return true; //shows everything
-          });
-          return false;
-      };
+    var tweten = document.getElementById('twenty-ten');
+    var all = document.getElementById('ever');
 
-  });*/
+    tweten.onclick = function(e) {
+        all.className = '';
+        this.className = 'active';
+        // The filter function takes a GeoJSON feature object
+        // and returns true to show it or false to hide it.
+        markerLayer.filter(function(f) {
+            return f.properties['start_year'] === '2010';
+            //return f.properties['start_year'] == 2010;
+        });
+        return false;
+    };
 
-  //map.addLayer(markerLayer);
+    all.onclick = function() {
+        tweten.className = '';
+        this.className = 'active';
+        markerLayer.filter(function(f) {
+            return true; //shows everything
+        });
+        return false;
+    };
 
-  // Attribute map
-  map.ui.attribution.add()
-      .content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');
+});
+map.addLayer(markerLayer);*/

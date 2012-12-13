b28e63ccda51d705da0b1e3904a924ef5198463e
Caroline <ccragin90@gmail.com>
2012-12-13 00:21:33 -0800

    try testing geojson

--- filter.js
+++ filter.js
@@ -5,25 +5,31 @@ mapbox.markers.interaction(markerLayer);
 map.addLayer(markerLayer);
 map.zoom(2).center({lat:25, lon:-43});
 
-markerLayer.add_feature({
-  id:"12 Lebanon St, Hamilton, NY, USA",
-  properties:{
-    "num_months":16,
-    "Type":"Caroline",
-    "start_month":1,
-    "start_year":2011,
-    "geo_accuracy":"apartment",
-    "Location":"12 Lebanon St, Hamilton, NY, USA",
-    "geo_longitude":-75.5452473843546,
-    "geo_latitude":42.8266391930919,
-    "num_cragins":1
-  },
-  type:"Feature",
-  geometry:{
-    "type":"Point",
-    "coordinates":[-75.5452473843546,42.8266391930919]
-  }
+markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson', function (err, features) {
+  if (err)
+    markerLayer.add_feature({
+      id:"12 Lebanon St, Hamilton, NY, USA",
+      properties:{
+        "num_months":16,
+        "Type":"Caroline",
+        "start_month":1,
+        "start_year":2011,
+        "geo_accuracy":"apartment",
+        "Location":"12 Lebanon St, Hamilton, NY, USA",
+        "geo_longitude":-75.5452473843546,
+        "geo_latitude":42.8266391930919,
+        "num_cragins":1
+      },
+      type:"Feature",
+      geometry:{
+        "type":"Point",
+        "coordinates":[-75.5452473843546,42.8266391930919]
+      }
+    });
+  else
+    markers.add_feature(features[1]);
 });
+//map.addLayer(markerLayer);?
       //markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson');
   /*var markerLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'), function() {
 

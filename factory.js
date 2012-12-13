b21ab4a2a46f1e6aa235b8afcfce2d59c52e7401
Caroline <ccragin90@gmail.com>
2012-12-13 15:24:06 -0800

    add factory.js

---
+++ factory.js
@@ -0,0 +1,20 @@
+var map = mapbox.map('map');
+map.addLayer(mapbox.layer().id('crags617.cragin_homes_blnk'));
+
+var markerLayer = mapbox.markers.layer();
+markerLayer.url('../../family/homesv5.geojson')
+		   .factory( function(homes) {
+	var img = document.createElement('img');
+	img.className = '.marker-image';
+	img.setAttribute('src', 'http://dl.dropbox.com/u/28523686/mHouse-24.png');
+	return img;
+});
+
+map.addLayer(markerLayer)
+//   .setExtent(markerLayer.extent());
+
+
+map.zoom(2).center({lat:25, lon:-43});
+
+map.ui.attribution.add()
+				  .content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');

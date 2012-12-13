31c926c802ad8ec8a0dca661b7eda3119542ddde
Caroline <ccragin90@gmail.com>
2012-12-13 01:37:48 -0800

    try simple filtering

--- filter.js
+++ filter.js
@@ -50,6 +50,34 @@ var myobj = [{"id":"12 Lebanon St, Hamilton, NY, USA","properties":{"num_months"
 map.addLayer(mapbox.layer().id('http://a.tiles.mapbox.com/v3/crags617.cragin_homes_blnk.jsonp'));
 var markerLayer = mapbox.markers.layer();
 mapbox.markers.interaction(markerLayer);
-markerLayer.features(myobj);
+markerLayer.features(myobj, function() {
+
+    map.extent(markerLayer.extent());
+
+    var tweten = document.getElementById('twenty-ten');
+    var all = document.getElementById('ever');
+
+    tweten.onclick = function(e) {
+        all.className = '';
+        this.className = 'active';
+        // The filter function takes a GeoJSON feature object
+        // and returns true to show it or false to hide it.
+        markerLayer.filter(function(f) {
+            return f.properties['start_year'] === '2010';
+            //return f.properties['start_year'] == 2010;
+        });
+        return false;
+    };
+
+    all.onclick = function() {
+        tweten.className = '';
+        this.className = 'active';
+        markerLayer.filter(function(f) {
+            return true; //shows everything
+        });
+        return false;
+    };
+
+});
 map.addLayer(markerLayer);
 map.zoom(2).center({lat:25, lon:-43});

21a6438d1c2713726fc5e865182fe6d9a0f9703e
Caroline <ccragin90@gmail.com>
2012-12-13 15:44:55 -0800

    remove zoom/pan handlers

--- factory.js
+++ factory.js
@@ -1,20 +1,24 @@
-var map = mapbox.map('map');
-map.addLayer(mapbox.layer().id('crags617.cragin_homes_blnk'));
+//create base layer
+var base = mapbox.layer().id('crags617.cragin_homes_blnk');
 
+//create marker layer
 var markerLayer = mapbox.markers.layer();
-markerLayer.url('../../family/homesv5.geojson')
+markerLayer.features(myobj["features"])
 		   .factory( function(homes) {
-             var img = document.createElement('img');
-             img.className = '.marker-image';
-             img.setAttribute('src', 'http://dl.dropbox.com/u/28523686/mHouse-24.png');
-             return img;
-           });
+	var img = document.createElement('img');
+	img.className = '.marker-image';
+	img.setAttribute('src', 'http://dl.dropbox.com/u/28523686/mHouse-24.png');
+	return img;
+});
+
+//create map with no handlers (no zoom/pan)
+var map = mapbox.map('map', layer, null, []); 
+//OR var map = mapbox.map('map', [layer, markerLayer], null, []); 
+
+map.centerzoom({lat: 25, lon: -43 }, 2);
 
 map.addLayer(markerLayer)
    .setExtent(markerLayer.extent());
-
-
-map.zoom(2).center({lat:25, lon:-43});
 
 map.ui.attribution.add()
 				  .content('<a href="http://mapbox.com/about/maps">Terms &amp; Feedback</a>');

873bac72f44600242a66fe439015d470d8c56d7c
Caroline <ccragin90@gmail.com>
2012-12-13 00:11:23 -0800

    try adding 1 marker

--- filter.js
+++ filter.js
@@ -1,11 +1,33 @@
 var map = mapbox.map('map');
+map.addLayer(mapbox.layer().id('http://a.tiles.mapbox.com/v3/crags617.cragin_homes_blnk.jsonp'));
+var markerLayer = mapbox.markers.layer();
+mapbox.markers.interaction(markerLayer);
+map.addLayer(markerLayer);
+map.zoom(2).center({lat:25, lon:-43});
 
-  map.addLayer(mapbox.layer().id('http://a.tiles.mapbox.com/v3/crags617.cragin_homes_blnk.jsonp'));
+markerLayer.add_feature({
+  id:"12 Lebanon St, Hamilton, NY, USA",
+  properties:{
+    "num_months":16,
+    "Type":"Caroline",
+    "start_month":1,
+    "start_year":2011,
+    "geo_accuracy":"apartment",
+    "Location":"12 Lebanon St, Hamilton, NY, USA",
+    "geo_longitude":-75.5452473843546,
+    "geo_latitude":42.8266391930919,
+    "num_cragins":1
+  },
+  type:"Feature",
+  geometry:{
+    "type":"Point",
+    "coordinates":[-75.5452473843546,42.8266391930919]
+  }
+});
+      //markerLayer.url('http://dl.dropbox.com/u/28523686/homes_v5.geojson');
+  /*var markerLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'), function() {
 
-  var markersLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson');
-  /*var markersLayer = mapbox.markers.layer().url('http://dl.dropbox.com/u/28523686/homes_v5.geojson'), function() {
-
-      map.extent(markersLayer.extent());
+      map.extent(markerLayer.extent());
 
       var tweten = document.getElementById('twenty-ten');
       var all = document.getElementById('ever');
@@ -15,7 +37,7 @@ var map = mapbox.map('map');
           this.className = 'active';
           // The filter function takes a GeoJSON feature object
           // and returns true to show it or false to hide it.
-          markersLayer.filter(function(f) {
+          markerLayer.filter(function(f) {
               return f.properties['start_year'] === '2010';
               //return f.properties['start_year'] == 2010;
           });
@@ -25,7 +47,7 @@ var map = mapbox.map('map');
       all.onclick = function() {
           tweten.className = '';
           this.className = 'active';
-          markersLayer.filter(function(f) {
+          markerLayer.filter(function(f) {
               return true; //shows everything
           });
           return false;
@@ -33,7 +55,7 @@ var map = mapbox.map('map');
 
   });*/
 
-  map.addLayer(markersLayer);
+  map.addLayer(markerLayer);
 
   // Attribute map
   map.ui.attribution.add()
